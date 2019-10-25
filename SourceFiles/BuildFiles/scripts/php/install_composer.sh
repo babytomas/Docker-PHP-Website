a72cf1e6e23f7c0b0e3425049a19e6c86df1a77a
Cheuknang, Chiang <mail@cheuknang.cn>
2019-10-25 23:55:39 +0800

    update composer script

---
+++ SourceFiles/BuildFiles/scripts/php/install_composer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Move composer
+mv /home/init-scripts/php/composer.phar /usr/bin/composer
+chmod +x /usr/bin/composer
+
+# Change the composer mirror
+composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
