e1c74f0da0f59530fcfc2c94a076ad57c46248bf
Cheuknang, Chiang <mail@cheuknang.cn>
2019-10-25 21:42:22 +0800

    update docker-composer config & dockerfile

--- SourceFiles/BuildFiles/scripts/php/install_ioncube.sh
+++ SourceFiles/BuildFiles/scripts/php/install_ioncube.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo -e "[ionCube Loader]\nzend_extension=\"/usr/local/etc/php/ioncube/ioncube_loader_lin_${1}.so\"" >> /usr/local/etc/php/php.ini
+PHP_VERSION=`php -v | grep -o -m 1 "[0-9].[0-9]" | head -1`
+
+gzip -d ioncube_loaders_lin_x86-64.tar.gz
+tar -xf ioncube_loaders_lin_x86-64.tar -C /usr/local/etc/php/
+
+echo -e "[ionCube Loader]\nzend_extension=\"/usr/local/etc/php/ioncube/ioncube_loader_lin_${PHP_VERSION}.so\"" >> /usr/local/etc/php/php.ini
