afc2686337c9cbb25d0908af3238544308fd8e9e
Cheuknang, Chiang <mail@cheuknang.cn>
2020-08-31 00:38:30 +0800

    Upgrade ionCube loader & install shell

--- SourceFiles/BuildFiles/scripts/php/install_ioncube.sh
+++ SourceFiles/BuildFiles/scripts/php/install_ioncube.sh
@@ -8,4 +8,8 @@ gzip -d ioncube_loaders_lin_x86-64.tar.gz
 tar -xf ioncube_loaders_lin_x86-64.tar -C /usr/local/etc/php/
 
 # Set the ioncube extension
-echo -e "[ionCube Loader]\nzend_extension=\"/usr/local/etc/php/ioncube/ioncube_loader_lin_${PHP_VERSION}.so\"" >> /usr/local/etc/php/php.ini
+# echo -e "[ionCube Loader]\nzend_extension=\"/usr/local/etc/php/ioncube/ioncube_loader_lin_${PHP_VERSION}.so\"" >> /usr/local/etc/php/php.ini
+cat <<EOF >> /usr/local/etc/php/php.ini
+[ionCube Loader]
+zend_extension="/usr/local/etc/php/ioncube/ioncube_loader_lin_$PHP_VERSION.so"
+EOF
